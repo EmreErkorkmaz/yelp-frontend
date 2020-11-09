077bfb263d1dbb3a1ccb9e22a926675cdac4ff99
Emre Erkorkmaz <erkorkmazemre77@gmail.com>
2020-11-09 19:46:09 +0300

    logo updates

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { TopNav } from "./TopNav/TopNav";
+import logo from '../assets/yelp.png';
+import styles from './LandingPage.module.css';
 
 export const LandingPage = (props) => {
   return (
     <div>
       <TopNav />
+      <img src={logo} className={styles.logo} alt='logo'/>
     </div>
   );
 };
