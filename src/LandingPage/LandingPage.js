e9f98f1223f73fc2c28798fa40fd9ea146104a35
Emre Erkorkmaz <erkorkmazemre77@gmail.com>
2020-11-10 14:49:16 +0300

    search bar styling

--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import { TopNav } from "./TopNav/TopNav";
 import logo from '../assets/yelp.png';
 import styles from './LandingPage.module.css';
+import { SearchBar } from "../SearchBar/SearchBar";
 
 export const LandingPage = (props) => {
   return (
     <div>
       <TopNav />
       <img src={logo} className={styles.logo} alt='logo'/>
+      <SearchBar/>
     </div>
   );
 };
